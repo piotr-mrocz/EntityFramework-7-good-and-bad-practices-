488f012de16e9fc23e62968ff3db5bcacf56b1a8
Piotr Mroczkowski <p.mrocz@vp.pl>
2022-11-28 20:14:47 +0100

    Add migrations with table Products

--- EntityFrameworkNews/EntityFrameworkNews/Migrations/20221128191349_AddProductsTable.Designer.cs
+++ EntityFrameworkNews/EntityFrameworkNews/Migrations/20221128191349_AddProductsTable.Designer.cs
@@ -3,6 +3,7 @@ using EntityFrameworkNews.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EntityFrameworkNews.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221128191349_AddProductsTable")]
+    partial class AddProductsTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
