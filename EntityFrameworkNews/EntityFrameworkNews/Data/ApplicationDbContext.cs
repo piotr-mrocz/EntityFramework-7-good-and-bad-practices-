486fd6ad55555893eae39178eef26babaca1483a
Piotr Mroczkowski <p.mrocz@vp.pl>
2022-11-28 20:13:08 +0100

    Add Products table

--- EntityFrameworkNews/EntityFrameworkNews/Data/ApplicationDbContext.cs
+++ EntityFrameworkNews/EntityFrameworkNews/Data/ApplicationDbContext.cs
@@ -13,6 +13,7 @@ public class ApplicationDbContext : DbContext, IApplicationDbContext
     #region DbSets
 
     public DbSet<User> Users { get; set; }
+    public DbSet<Product> Products { get; set; }
 
     #endregion DbSets
 
---
+++ EntityFrameworkNews/EntityFrameworkNews/Models/Entities/Product.cs
@@ -0,0 +1,7 @@
+﻿namespace EntityFrameworkNews.Models.Entities;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string ProductName { get; set; } = null!;
+}
